3dca72503112b0c1a2df53c17ba49c0f0940b591
Md Taufik Khan <147173391+kmdtaufik@users.noreply.github.com>
2025-12-09 14:55:56 +0600

    chore: cleanup,add monitor speed

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     
   };
 
-  outputs = { self, nixpkgs, flake-utils, antigravity-nix, ... }:
+  outputs = { self, nixpkgs, flake-utils,  ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
